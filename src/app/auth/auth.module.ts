0f6b19e5f42b613c14bf876f25f663e8e13adcf8
gotrecillo <sergio.panadero.perez@gmail.com>
2017-01-07 20:20:29 +0100

    login form started

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports : [
     CommonModule,
     SharedModule,
     AuthRoutingModule,
+    ReactiveFormsModule,
   ],
 
   declarations : [ LoginComponent ],
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'piz-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector    : 'piz-login',
+  templateUrl : './login.component.html',
+  styleUrls   : [ './login.component.scss' ],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  user = { email : '', password : '' };
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm() {
+    this.loginForm = this.fb.group({
+      email : [
+        this.user.email, [
+          Validators.required,
+        ],
+      ],
+
+      password : [
+        this.user.password, [
+          Validators.required,
+        ],
+      ],
+    });
+
+    this.loginForm.valueChanges.subscribe(data => console.log(data));
   }
 
 }
